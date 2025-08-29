86742d5c833944cd11cf9b62107bf78bf387e62c
강은서 <gang-eunseo@gang-eunseoui-MacBookAir.local>
2025-08-30 02:17:19 +0900

    add boj_10872 solution

--- 2025-08/boj_10872/silverkkang.py
+++ 2025-08/boj_10872/silverkkang.py
@@ -1,3 +1,13 @@
 # silverkkang
 print("이정도좆밥")
 print("오늘 풀고 내일도 한문제 ㄱ")
+
+n = int(input())
+
+def factorial(n):
+    k = 1
+    for i in range(1, n+1):
+        k = k*i
+    return k
+
+print(factorial(n))
